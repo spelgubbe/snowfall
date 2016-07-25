b261ea9090632bd903f466ab789886440220fdcb
unknown <jakob.kratz@hotmail.com>
2016-07-26 00:38:29 +0200

    Struggle (1/5 bars have animations)

--- Snowfall.lua
+++ Snowfall.lua
@@ -1,87 +1,87 @@
+--[[
+
+	Problems: when an ability/macro is present (except on first actionbar) it doesn't flash,
+	when you remove the ability/macro however, it flashes (what?!)
+
+]]
+
 local animationsCount, animations = 5, {}
 local animationNum = 1
 local replace = string.gsub
 local frame, texture, animationGroup, alpha1, scale1, scale2, rotation2
 
 for i = 1, animationsCount do
-frame = CreateFrame("Frame")
+	frame = CreateFrame("Frame")
 
-texture = frame:CreateTexture()
-texture:SetTexture([[Interface\Cooldown\star4]])
-texture:SetAlpha(0)
-texture:SetAllPoints()
-texture:SetBlendMode("ADD")
-animationGroup = texture:CreateAnimationGroup()
+	texture = frame:CreateTexture()
+	texture:SetTexture([[Interface\Cooldown\star4]])
+	texture:SetAlpha(0)
+	texture:SetAllPoints()
+	texture:SetBlendMode("ADD")
+	animationGroup = texture:CreateAnimationGroup()
 
-alpha1 = animationGroup:CreateAnimation("Alpha")
-alpha1:SetFromAlpha(0)
-alpha1:SetToAlpha(1)
-alpha1:SetDuration(0)
-alpha1:SetOrder(1)
+	alpha1 = animationGroup:CreateAnimation("Alpha")
+	alpha1:SetFromAlpha(0)
+	alpha1:SetToAlpha(1)
+	alpha1:SetDuration(0)
+	alpha1:SetOrder(1)
 
-scale1 = animationGroup:CreateAnimation("Scale")
-scale1:SetScale(1.5, 1.5)
-scale1:SetDuration(0)
-scale1:SetOrder(1)
+	scale1 = animationGroup:CreateAnimation("Scale")
+	scale1:SetScale(1.5, 1.5)
+	scale1:SetDuration(0)
+	scale1:SetOrder(1)
 
-scale2 = animationGroup:CreateAnimation("Scale")
-scale2:SetScale(0, 0)
-scale2:SetDuration(0.3)
-scale2:SetOrder(2)
+	scale2 = animationGroup:CreateAnimation("Scale")
+	scale2:SetScale(0, 0)
+	scale2:SetDuration(0.3)
+	scale2:SetOrder(2)
 
-rotation2 = animationGroup:CreateAnimation("Rotation")
-rotation2:SetDegrees(90)
-rotation2:SetDuration(0.3)
-rotation2:SetOrder(2)
+	rotation2 = animationGroup:CreateAnimation("Rotation")
+	rotation2:SetDegrees(90)
+	rotation2:SetDuration(0.3)
+	rotation2:SetOrder(2)
 
-animations[i] = {frame = frame, animationGroup = animationGroup}
+	animations[i] = {frame = frame, animationGroup = animationGroup}
 end
 
 local animate = function(button)
-if not button:IsVisible() then
+	if not button:IsVisible() then
+		return true
+	end
+	local animation = animations[animationNum]
+	local frame = animation.frame
+	local animationGroup = animation.animationGroup
+	frame:SetFrameStrata(button:GetFrameStrata())
+	frame:SetFrameLevel(button:GetFrameLevel() + 10)
+	frame:SetAllPoints(button)
+	animationGroup:Stop()
+	animationGroup:Play()
+	animationNum = (animationNum % animationsCount) + 1
 	return true
-end
-local animation = animations[animationNum]
-local frame = animation.frame
---print(frame)
-local animationGroup = animation.animationGroup
-frame:SetFrameStrata(button:GetFrameStrata())
---print(button:GetFrameStrata()) -- inte felet
---print(frame:GetFrameStrata()) --
-frame:SetFrameLevel(button:GetFrameLevel() + 10)
-frame:SetAllPoints(button)
-animationGroup:Stop()
-animationGroup:Play()
-animationNum = (animationNum % animationsCount) + 1
---print("Num animations: "..animationNum)
-print('Animate ran successfully')
-return true
 end
 
 -- didn't run on PLAYER_ENTERING_WORLD
 --hooksecurefunc('ActionButton_UpdateHotkeys', function(button, buttonType)
 hooksecurefunc('ActionButton_Update', function(button, buttonType)
-if InCombatLockdown() then return end -- no animation while in CC
-if not button.hooked then
-local id, actionButtonType, key
-if not actionButtonType then
-actionButtonType = button:GetAttribute('binding') or string.upper(button:GetName())
+	if InCombatLockdown() then return end -- no animation while in CC
+	if not button.hooked then
+		local id, actionButtonType, key
+		if not actionButtonType then
+			actionButtonType = button:GetAttribute('binding') or string.upper(button:GetName())
 
-actionButtonType = replace(actionButtonType, 'BOTTOMLEFT', '1')
-actionButtonType = replace(actionButtonType, 'BOTTOMRIGHT', '2')
-actionButtonType = replace(actionButtonType, 'RIGHT', '3')
-actionButtonType = replace(actionButtonType, 'LEFT', '4')
-actionButtonType = replace(actionButtonType, 'MULTIBAR', 'MULTIACTIONBAR')
---print(actionButtonType)
-end
-local key = GetBindingKey(actionButtonType)
-if key then
---print('hey this runs!'..key)
-button:RegisterForClicks("AnyDown")
-SetOverrideBinding(button, true, key, 'CLICK '..button:GetName()..':LeftButton')
-end
-button.AnimateThis = animate
-SecureHandlerWrapScript(button, "OnClick", button, [[ control:CallMethod("AnimateThis", self) ]])
-button.hooked = true
-end
+			actionButtonType = replace(actionButtonType, 'BOTTOMLEFT', '1')
+			actionButtonType = replace(actionButtonType, 'BOTTOMRIGHT', '2')
+			actionButtonType = replace(actionButtonType, 'RIGHT', '3')
+			actionButtonType = replace(actionButtonType, 'LEFT', '4')
+			actionButtonType = replace(actionButtonType, 'MULTIBAR', 'MULTIACTIONBAR')
+		end
+		local key = GetBindingKey(actionButtonType)
+		if key then
+			button:RegisterForClicks("AnyDown")
+			SetOverrideBinding(button, true, key, 'CLICK '..button:GetName()..':LeftButton')
+		end
+		button.AnimateThis = animate
+		SecureHandlerWrapScript(button, "OnClick", button, [[ control:CallMethod("AnimateThis", self) ]])
+		button.hooked = true	
+	end
 end)
